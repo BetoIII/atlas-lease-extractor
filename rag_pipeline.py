422dbdf0904ea175de7a23d826216d25029a8dce
Beto Juarez III <betoiii@gmail.com>
2025-06-02 11:50:02 -0700

    rag index and retriever now use llamaparse

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,14 +1,20 @@
 import os
-from multiprocessing import Lock
-from llama_index.core import Document, SimpleDirectoryReader, VectorStoreIndex, load_index_from_storage
-from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
-from llama_index.llms.openai import OpenAI
 from dotenv import load_dotenv
-from typing import List
-from llama_parse import LlamaParse
 from llama_cloud.client import LlamaCloud
 from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
 from llama_index.core import Document
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import SimpleDirectoryReader
+
+pipeline = IngestionPipeline(
+    transformations=[
+        SentenceSplitter(),
+        HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
+    ],
+    docstore=SimpleDocumentStore(),
+)
 
 # Load environment variables from .env file
 load_dotenv()
@@ -25,9 +31,7 @@ index = LlamaCloudIndex(
     api_key=os.environ.get("LLAMA_CLOUD_API_KEY")
 )
 
-documents = LlamaParse(result_type="markdown").load_data(
-    file_path="/Users/betojuareziii/Applications/atlas-lease-extractor/uploaded_documents/2019-2022-Lease-Agreement-TETCO-Signed-tiny.pdf"
-)    
+documents = SimpleDirectoryReader("/Users/betojuareziii/Applications/atlas-lease-extractor/uploaded_documents", filename_as_id=True).load_data()
 
 client = LlamaCloud(
     token=os.environ.get("LLAMA_CLOUD_API_KEY"),
@@ -46,3 +50,7 @@ upserted_doc = client.pipelines.upsert_batch_pipeline_documents(
 )
 
 print(upserted_doc)
+nodes = pipeline.run(documents=documents)
+print(f"Ingested {len(nodes)} Nodes")
+for node in nodes:
+    print(f"Node: {node.text}")
