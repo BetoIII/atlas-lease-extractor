62e42958211c9e27b55d1e1a1756779870b37a5e
Beto Juarez III <betoiii@gmail.com>
2025-07-21 19:51:27 -0700

    fix build error

--- flask_react/app/layout.tsx
+++ flask_react/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { AutumnProvider } from "better-auth/react";
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -16,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AutumnProvider betterAuthUrl={process.env.NEXT_PUBLIC_BETTER_AUTH_URL}>
-          {children}
-        </AutumnProvider>
+        {children}
       </body>
     </html>
   )
